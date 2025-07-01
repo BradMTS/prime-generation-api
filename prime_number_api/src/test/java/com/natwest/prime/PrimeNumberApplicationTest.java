f40673ccd83f658aa15bc6bc7d1687942430a052
BradMTS <badalvishal.in@gmail.com>
2025-07-01 19:14:18 +0530

    Update PrimeNumberApplicationTest.java

--- prime_number_api/src/test/java/com/natwest/prime/PrimeNumberApplicationTest.java
+++ prime_number_api/src/test/java/com/natwest/prime/PrimeNumberApplicationTest.java
@@ -3,12 +3,11 @@ package com.natwest.prime;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest // Loads the full Spring application context
+@SpringBootTest
 class PrimeNumberApplicationTest {
 
     @Test
     void contextLoads() {
-        // This test simply checks if the Spring application context loads successfully.
-        // If it loads without exceptions, it means the basic setup is correct.
+        // Checks if the Spring application context loads successfully without exceptions, it means the basic setup is correct.
     }
 }
